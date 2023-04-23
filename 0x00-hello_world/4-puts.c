934b130b86352108c1d709e70573cdbb21c17ca5
elissianashaat <elissianashaat@icloud.com>
2023-04-23 12:32:58 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/*this is the main function and the puts task*/
-int main (void)
+/**
+* Main - A  program that prints a line with puts function
+* Return: 0 (success)
+*/
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
