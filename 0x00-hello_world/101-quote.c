83da4a621e92ed1ad99b97e310d067068fb9fda2
elissianashaat <elissianashaat@icloud.com>
2023-04-23 14:11:10 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,6 @@
 int main(void)
 {
 char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-size_t size = sizeof(arr) / sizeof(arr[0]);
-for (int i = 0; i < size; i++)
-{
-putchar(arr[i]);
-}
-return (0);
+write(1,arr,59);
+return (1);
 }
