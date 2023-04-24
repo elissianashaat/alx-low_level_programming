ec032abb513cd33ec477b15fa79490e568b8e9c2
elissianashaat <elissianashaat@icloud.com>
2023-04-24 11:49:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print comb of numbers'
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+int i = 'a';
+while (i < 'z')
+{
+putchar(i);
+i++;
+}
+putchar("\n");
+return (0);
+}
