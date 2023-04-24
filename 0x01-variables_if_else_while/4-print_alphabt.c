7ce00d54db24e167527a5c71ca62dd1b43f15fd7
elissianashaat <elissianashaat@icloud.com>
2023-04-24 12:09:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print comb of numbers'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 'a';
+while (i <= 'z')
+{
+if (i == 'q' || i == 'e')
+{
+break;
+}
+else
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
