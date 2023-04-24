869de185de89de2749360de8a5176e5f5939efda
elissianashaat <elissianashaat@icloud.com>
2023-04-24 13:45:59 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print comb of numbers'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 'A';
+while (i < 'G')
+{
+putchar(i);
+i++;
+}
+int z = 0;
+while (z < 10)
+{
+putchar(z);
+z++;
+}
+putchar('\n');
+return (0);
+}
