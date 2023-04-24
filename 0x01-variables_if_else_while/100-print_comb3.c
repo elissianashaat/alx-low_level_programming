affc831ac6d2ac8a98d02fd73305e434f2a28a8e
elissianashaat <elissianashaat@icloud.com>
2023-04-24 16:17:23 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,19 +5,17 @@
 * Return: Always 0 (Success)
 */
 
-int main(void)
+int main(void) 
 {
 int i = 0;
+while (i < 10){
 int j = 0;
-while (i < 10)
-{
-while (j < 10)
-{
-if (i < j && i != j)
+while (j < 10){
+if (i != j && i < j)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i + j != 17)
+if (j + i != 17)
 {
 putchar(',');
 putchar(' ');
