69c810c8eb259dde6cc79d04f0d1d343a0a19f56
elissianashaat <elissianashaat@icloud.com>
2023-04-24 12:52:25 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print comb of numbers'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+putchar('\n');
+return (0);
+}
