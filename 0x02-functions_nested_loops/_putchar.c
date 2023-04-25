6bf618d567b7fc897dffdecced42712b417c4046
elissianashaat <elissianashaat@icloud.com>
2023-04-25 11:25:06 -0700

    _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -6,7 +6,7 @@
  * @c: the character to print
  *
  * Return: on success 1.
- * on error, -1 is returned, and errno is set appropriatelu
+ * on error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
