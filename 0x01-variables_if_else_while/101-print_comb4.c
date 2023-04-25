42e46727cbb24438a4ba5c990a53e32b9b56cd6f
elissianashaat <elissianashaat@icloud.com>
2023-04-24 18:27:16 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print comb of numbers'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+int y = 0;
+while (y < 10)
+{
+int z = 0;
+while (z < 10)
+{
+if (x != y && y != z && z != x && x < y < z)
+{
+putchar(x + '0');
+putchar(y + '0');
+putchar(z + '0');
+if (x + y + z != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+z++;
+}
+y++;
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
