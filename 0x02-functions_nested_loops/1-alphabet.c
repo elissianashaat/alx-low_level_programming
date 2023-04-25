3e1198117e326701350c7f8ba848c4d4c03ca751
elissianashaat <elissianashaat@icloud.com>
2023-04-25 12:15:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ * the alphabet from a-z
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int i = 'a';
+for (int i = 'a'; z <= 'z'; z++)
+{
+_putchar(z);
+_putchar('\n');
+}
+return (0);
+}
