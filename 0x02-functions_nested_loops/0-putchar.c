b8f97d00b0e4fabe98c9ffc44b84f98d53b690dc
elissianashaat <elissianashaat@icloud.com>
2023-04-25 11:34:42 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main- Entry point
+ * Description: prints _putchar using putchar prototype
+ * Return: always 0
+ */
+
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < str.strlen(); ch++)
+{
+_putchar(str[ch]);
+}
+_putchar('\n');
+return (0);
+}
