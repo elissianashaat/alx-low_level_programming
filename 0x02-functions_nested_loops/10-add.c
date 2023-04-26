cbfb4fa6c169ff813550ab31c3373769329f03b8
elissianashaat <elissianashaat@icloud.com>
2023-04-26 02:49:41 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: first input
+ * @b: second input
+ * Return: sum of a&b
+ */
+
+int add(int a, int b)
+{
+int res = a + b;
+return (res);
+}
