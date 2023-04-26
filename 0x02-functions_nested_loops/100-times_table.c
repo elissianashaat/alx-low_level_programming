0474742cf7e146fd6032138eff9c6959e96c2a8a
elissianashaat <elissianashaat@icloud.com>
2023-04-26 06:09:10 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,20 +18,24 @@ for (mult = 1; mult <= n; mult++)
 _putchar(',');
 _putchar(' ');
 prod = mult * num;
-if (prod <= 9 || prod <= 99)
+if (prod <= 9)
+{
+_putchar(' ');
+}
+if (prod <= 99)
 {
 _putchar(' ');
 }
 if (prod >= 100)
 {
-_putchar((prod / 100)) + '0');
-_putchar((prod / 10)) % 10 + '0');
+_putchar((prod / 100) + '0');
+_putchar((prod / 10) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
-_putchar((prod / 10)) + '0');
+_putchar((prod / 10) + '0');
 }
-_putchar((prod % 10)) + '0');
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
