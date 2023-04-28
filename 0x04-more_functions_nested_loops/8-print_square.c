7215f6d2b017e6c923cb52a9e36549336e856d14
elissianashaat <elissianashaat@icloud.com>
2023-04-28 07:13:40 -0700

    sq

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using the character #
+ * @size: is the size of the square
+ * Return: 0 always
+ */
+
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
