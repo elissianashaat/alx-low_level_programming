ad151610c93f682976d37baee44a823e7bd167ea
elissianashaat <elissianashaat@icloud.com>
2023-05-02 15:31:57 -0700

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - function that prints every
+ * other character of a string
+ * @str: string to be printed as characters
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
