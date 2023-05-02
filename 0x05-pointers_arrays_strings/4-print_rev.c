6bf39db4c8663b696c9d15d1611846de96069a28
elissianashaat <elissianashaat@icloud.com>
2023-05-02 15:01:13 -0700

    as

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - function that prints string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+
+void print_rev(char *str)
+{
+int i = 0;
+while (str[i] != 0)
+{
+i++;
+}
+while (i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
