f6321228d9c16d7a15fef4258213c36471ad07d1
elissianashaat <elissianashaat@icloud.com>
2023-05-02 10:37:46 -0700

    aa

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * reset_to_98 - takes a pointer as int 
+ * updates the value it points to to 98
+ * @n: an input integer pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointer read me
