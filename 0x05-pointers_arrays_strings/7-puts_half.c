3ae9bdba2ae29e37d1c94ae7886d77ed55022d57
elissianashaat <elissianashaat@icloud.com>
2023-05-02 16:33:48 -0700

    sss

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half -prints half string
+ * @str: string to be splitted
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int i;
+int j = 0;
+while (str[j] != '\0')
+{
+j++
+}
+for (i = 0; i < j/2; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
