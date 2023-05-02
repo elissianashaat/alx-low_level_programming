a20accd40a32ddc81342594b6c080fc12e1e66b6
elissianashaat <elissianashaat@icloud.com>
2023-05-02 16:14:07 -0700

    sss

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,14 @@
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i ++)
+int j = 0;
+while (str[j] != '\0')
 {
-if (i % 2 == 0)
+j++;
+}
+for (i = 0; i < j; i+=2)
 {
 _putchar(str[i]);
-}
 }
 _putchar('\n');
 }
