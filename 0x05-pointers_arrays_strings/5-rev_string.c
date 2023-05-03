4ceb8272b34ba9645db53a9eeaf457f3e37945ee
elissianashaat <elissianashaat@icloud.com>
2023-05-02 19:41:02 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,4 +10,16 @@
 
 void rev_string(char *s)
 {
-
+int l = 0; i = 0;
+char tmp;
+while (s[i])
+{
+l++;
+}
+for (i = l - 1; i >= l / 2; i--)
+{
+tmp = s[i];
+s[i] = s[l - i - 1];
+s[l - i - 1] = tmp;
+}
+}
