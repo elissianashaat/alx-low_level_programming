d34874bb02130680d8d5778ce4e745e6b964f273
elissianashaat <elissianashaat@icloud.com>
2023-05-04 16:38:38 -0700

    aaa

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,16 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
-while (dest[i] != '\0')
+while (i < n && src[i] != '\0')
 {
-dest[i] = 0;
+dest[i] = src[i];
 i++;
 }
-i = 0;
-while (src[j] != '\0' && j < n)
+while (i < n)
 {
-dest[i] = src[j];
-j++;
+dest[i] = '\0';
 i++;
 }
 return (dest);
