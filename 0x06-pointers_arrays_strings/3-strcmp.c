10fcd339263c7bcedffd7776768a2e6b2958c942
elissianashaat <elissianashaat@icloud.com>
2023-05-05 05:14:34 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,19 +12,13 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-int j = 0;
-while (s1[i] != '\0' && s2[j] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (i == j)
+if (s1[i] != s2[i])
 {
-return (1);
-}
-else
-{
-return (0);
+return (s1[i] - s2[i]);
 }
 i++;
-j++;
 }
 return (0);
 }
