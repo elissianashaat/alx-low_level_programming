d4b4bce2f29c3d5740a892b239813b4ffbaf5730
elissianashaat <elissianashaat@icloud.com>
2023-05-09 08:20:32 -0700

    a

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
  * print_diagsums - entry point
  * @a: pointer
  * @size: size
- * Return: always 0 
+ * Return: always 0
  */
 
 void print_diagsums(int *a, int size)
@@ -15,11 +15,11 @@ int sum1;
 int sum2;
 for (i = 0; i < size; i++)
 {
-sum1 = sum1 + a[y * size + y];
+sum1 = sum1 + a[i * size + i];
 }
 for (i = size - 1; i >= 0; i--)
 {
-sum2 +=  a[y * size + (size -  y - 1)];
+sum2 +=  a[i * size + (size -  i - 1)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
