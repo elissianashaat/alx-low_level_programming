20c875bc2b588e8eb2021e72147c5cb5caf46d81
elissianashaat <elissianashaat@icloud.com>
2023-05-09 07:38:37 -0700

    A

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,9 @@
 #include "main.h"
 
 /**
- *
+ * print_chessboard - entry point
+ * @a: array
+ * Return: always 0
  */
 
 void print_chessboard(char (*a)[8])
@@ -14,7 +16,7 @@ for (i = 0; i < 8; i++)
 for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
