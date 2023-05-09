db7a5bc9a17bd3678a79b35dd861ecf5c7ae34d8
elissianashaat <elissianashaat@icloud.com>
2023-05-09 07:19:35 -0700

    A

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-char *s = "hello, world";
-char *f = "world";
+char *s = "First, solve the problem. Then, write the code.";
+char *f = "solve";
 char *t;
 t = _strstr(s, f);
-printf("%s\n", t);
+printf("%s\n", (t == NULL ? "nil" : t));
 return (0);
 }
