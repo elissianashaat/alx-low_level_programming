1916d752fd6a6d88bb5ef39fd7aa50c25f695ebd
elissianashaat <elissianashaat@icloud.com>
2023-05-11 14:52:15 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry point
+ * @s: string to be reversed
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
