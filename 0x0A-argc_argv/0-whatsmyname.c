3c74d00b26610e9ac855e22161055a034fc85f85
elissianashaat <elissianashaat@icloud.com>
2023-05-12 19:07:21 -0700

    a' '
    git commit -m a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: value passed to arguments
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
