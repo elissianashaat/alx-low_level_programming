69654e2b99ca64d82394cb50125ee6157601085d
elissianashaat <elissianashaat@icloud.com>
2023-05-12 19:22:32 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: string value
+ * Return: 0
+ */
+
+int main(int argc, int *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
