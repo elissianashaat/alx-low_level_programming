df381460cd428d9fb1c54cc1be35010f686902eb
elissianashaat <elissianashaat@icloud.com>
2023-05-13 17:12:19 -0700

    A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,19 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 int num1;
 int num2;
 int result;
 if (argc != 3)
 {
 printf("Error\n");
-return  1;
+return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 printf("%d\n", result);
-return 0;
+return (0);
 }
