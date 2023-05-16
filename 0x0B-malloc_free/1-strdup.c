78549056e2d428b0d6ea2bd2da2815f2177eb51c
elissianashaat <elissianashaat@icloud.com>
2023-05-16 15:51:45 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - entry point
+ * @str: pointer
+ * Return: true
+ */
+
+char *_strdup(char *str)
+{
+int i;
+int j;
+char *m;
+i = 0;
+if (str == NULL)
+{
+return (NULL);
+}
+while (str[i] != '\0')
+{
+i++;
+}
+m = malloc(i * (sizeof(*str) + 1));
+if (m == 0)
+{
+return (NULL);
+}
+else
+{
+for (j = 0; str[j]; j++)
+{
+m[j] = str[j];
+}
+}
+return (m);
+}
