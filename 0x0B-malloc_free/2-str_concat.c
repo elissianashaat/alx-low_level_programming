00a7b1ec54c5bc071355da763ae5f0bae64eddb1
elissianashaat <elissianashaat@icloud.com>
2023-05-16 17:10:06 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,17 +12,9 @@
 
 char *str_concat(char *s1, char *s2)
 {
-int i;
-int j;
-int k;
-int s;
-int f;
+int i, j, k, s, f;
 char *s3;
-j = 0;
-i = 0;
-k = 0;
-s = 0;
-f = 0;
+j = i = k = s = f = 0;
 if (s1 == NULL)
 {
 s1 = "";
@@ -40,7 +32,7 @@ while (s2[j] != '\0')
 j++;
 }
 k = i + j;
-s3 = malloc((k * sizeof(char)) + 1);
+s3 = malloc((k *sizeof(char)) + 1);
 if (s3 == NULL)
 {
 return (NULL);
