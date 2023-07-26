d236f75a69a4b63b25e4c6b0d283b91f0439bb02
elissianashaat <elissianashaat@icloud.com>
2023-07-25 18:34:38 -0700

    as

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,8 +21,7 @@ return;
 va_start(ap, n);
 while (i--)
 {
-printf("%s%s", (s = va_arg(ap, char *)) ? s : "(nil)",
-i ? (sep ? sep : "") : "\n");
+printf("%s%s", (s = va_arg(ap, char *)) ? s : "(nil)", i ? (sep ? sep : "") : "\n");
 }
 va_end(ap);
 }
