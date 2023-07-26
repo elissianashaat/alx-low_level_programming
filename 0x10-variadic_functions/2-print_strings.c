b87b29e3b4508412507e833d097f5609029b2c83
elissianashaat <elissianashaat@icloud.com>
2023-07-25 18:22:02 -0700

    a

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function to prints strings
+ * @sep: seperator
+ * @n: no. of args
+ * @...: strings printed
+ * Return: void
+ */
+
+void print_strings(const char *sep, const unsigned int n, ...)
+{
+int i = n;
+char *s;
+va_list ap;
+if (!n)
+{
+printf("\n");
+return;
+}
+va_start(ap, n);
+while (i--)
+{
+printf("%s%s", (s = va_arg(ap, char *)) ? s : "(nil)",
+i ? (sep ? sep : "") : "\n");
+va_end(ap);
+}
+}
